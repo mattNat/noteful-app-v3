1553b162bc82593d49da344634ead4b486ae5d9a
mattNat <mattsanpedro76@gmail.com>
2018-02-15 09:19:46 -0800

    create routes/folders.js and account for in server.js

---
+++ routes/folders.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Folder = require('../models/folder');
+const Note = require('../models/note');
+
+// FOLDER ROUTER ENDPOINTS HERE
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ mongoose.Promise = global.Promise;
 const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
+const foldersRouter = require('./routes/folders');
 
 // Create an Express application
 const app = express();
@@ -25,6 +26,7 @@ app.use(express.json());
 
 // Mount router on "/api"
 app.use('/v3', notesRouter);
+app.use('/v3', foldersRouter);
 
 // Catch-all 404
 app.use(function (req, res, next) {
