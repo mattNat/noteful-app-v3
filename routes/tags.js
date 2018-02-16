7955ad28e73da6696a9b5a7efbf77c9a2d9036c3
mattNat <mattsanpedro76@gmail.com>
2018-02-16 09:11:54 -0800

    create /routes/tags.js

---
+++ routes/tags.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag');
+
+// ENDPOINTS GO HERE
+
+
+module.exports = router;
